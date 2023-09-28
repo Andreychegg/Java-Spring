64fc9e9a65a292c40739fa2448852275891a3700
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-09-28 18:26:28 +0500

    Add files via upload

---
+++ base/mod1/MinElement.java
@@ -0,0 +1,17 @@
+public class MinElement {
+    public static void main(String[] args) {
+        int[][] x = {{20, 34, 2}, {9, 12, 18}, {3, 4, 5}};
+
+        int min = x[0][0];
+
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[i].length; j++) {
+                if (x[i][j] < min) {
+                    min = x[i][j];
+                }
+            }
+        }
+
+        System.out.print("Минимальный элемент: " + min);
+    }
+}

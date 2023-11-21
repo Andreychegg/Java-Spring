c23c6a8942cd442f6438a2efc449701a8f11cd94
Andreychegg <126164512+Andreychegg@users.noreply.github.com>
2023-11-21 20:53:44 +0500

    Add files via upload

---
+++ buns/mod8/spring_course_springboot/src/test/java/com/andrey/spring/springboot/mod8/Mod8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey.spring.springboot.mod8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ buns/mod8/spring_data_rest/src/main/java/com/andrey/spring/springboot/spring_data_rest/entity/Employee.java
@@ -0,0 +1,84 @@
+package com.andrey.spring.springboot.spring_data_rest.entity;
+
+//import javax.persistence.*;
+//import javax.validation.constraints.Max;
+//import javax.validation.constraints.Min;
+//import javax.validation.constraints.NotBlank;
+//import javax.validation.constraints.Size;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+//    @Size(min = 2, message = "name must be min 2 symbols")
+    private String name;
+
+    @Column(name = "surname")
+//    @NotBlank(message = "surname is required field")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+//    @Min(value = 500, message = "must be greater than 499")
+//    @Max(value = 2000, message = "must be less than 2001")
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
